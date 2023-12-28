3dfd426ddc873c7589ea97d42cfaa8b4222d95db
raghda900 <raghda.soud90@gmail.com>
2023-12-28 19:12:40 +0200

    0

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 --displays the number of records with id = 89 in the table
-SELECT COUNT(id) AS 'count89' FROM first_table WHERE id=89;
+SELECT COUNT(id)  AS 'count89' FROM first_table WHERE id=89 ;
