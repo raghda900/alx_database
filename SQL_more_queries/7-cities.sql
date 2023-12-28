79d8568bdfaaa67fec39d10cf466ce344d702bda
raghda900 <raghda.soud90@gmail.com>
2023-12-28 19:58:13 +0200

    0

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- create database hbtn_0d_usa containing table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
